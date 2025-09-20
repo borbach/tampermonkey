b12a153a384c2cd82db40541ae6aa256292e8aa7
borbach <54514786+borbach@users.noreply.github.com>
2025-09-20 13:48:47 -0400

    Add files via upload

---
+++ Yahoo Mail Script-2025-09-19.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Yahoo Mail Script
+// @namespace    http://tampermonkey.net/
+// @version      2025-09-19
+// @description  try to take over the world!
+// @author       Bill
+// @match        https://mail.yahoo.com/n/inbox/all?.src=ym&reason=myc&listFilter=ALL_INBOX&accountIds=1
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=yahoo.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    alert("Please remember to delete all unnecessary emails immediately.");
+})();
